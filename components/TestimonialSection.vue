2fcf51f572e5e62b4fca8638663fd6c6059956bf
shuvo-me <shuvo.js.dev@gmail.com>
2024-01-15 13:45:19 +0600

    fix: design testimonial section

--- components/TestimonialSection.vue
+++ components/TestimonialSection.vue
@@ -1,148 +1,50 @@
 <template>
-  <div class="relative isolate bg-white pb-32 pt-24 sm:pt-32">
-    <div
-      class="absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl"
-      aria-hidden="true"
-    >
-      <div
-        class="ml-[max(50%,38rem)] aspect-[1313/771] w-[82.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]"
-        style="
-          clip-path: polygon(
-            74.1% 44.1%,
-            100% 61.6%,
-            97.5% 26.9%,
-            85.5% 0.1%,
-            80.7% 2%,
-            72.5% 32.5%,
-            60.2% 62.4%,
-            52.4% 68.1%,
-            47.5% 58.3%,
-            45.2% 34.5%,
-            27.5% 76.7%,
-            0.1% 64.9%,
-            17.9% 100%,
-            27.6% 76.8%,
-            76.1% 97.7%,
-            74.1% 44.1%
-          );
-        "
-      ></div>
+  <section
+    class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8"
+  >
+    <div class="text-center">
+      <h2
+        class="text-base font-semibold leading-8 tracking-tight text-blue-600"
+      >
+        A Symphony of Satisfaction
+      </h2>
+      <p
+        class="mt-2 text-2xl font-bold text-gray-900 sm:text-4xl !leading-[60px]"
+      >
+        Stories That Push Us Beyond Boundary
+      </p>
     </div>
     <div
-      class="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end"
-      aria-hidden="true"
-    >
-      <div
-        class="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] xl:ml-0 xl:mr-[calc(50%-12rem)]"
-        style="
-          clip-path: polygon(
-            74.1% 44.1%,
-            100% 61.6%,
-            97.5% 26.9%,
-            85.5% 0.1%,
-            80.7% 2%,
-            72.5% 32.5%,
-            60.2% 62.4%,
-            52.4% 68.1%,
-            47.5% 58.3%,
-            45.2% 34.5%,
-            27.5% 76.7%,
-            0.1% 64.9%,
-            17.9% 100%,
-            27.6% 76.8%,
-            76.1% 97.7%,
-            74.1% 44.1%
-          );
-        "
-      ></div>
-    </div>
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <div class="mx-auto max-w-xl text-center">
-        <h2
-          class="text-lg font-semibold leading-8 tracking-tight text-blue-600"
+      class="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20"
+    />
+    <div
+      class="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"
+    />
+    <div class="mx-auto max-w-2xl lg:max-w-4xl">
+      <figure class="mx-auto max-w-2xl">
+        <blockquote
+          class="mt-10 text-lg font-semibold leading-8 tracking-tight text-gray-700 sm:text-2xl sm:leading-9"
         >
-          A Symphony of Satisfaction
-        </h2>
-        <p
-          class="mt-2 text-2xl font-bold text-gray-900 sm:text-4xl !leading-[60px]"
-        >
-          Stories That Push Us <br />
-
-          <span
-            class="py-2 bg-clip-text bg-gradient-to-tl from-blue-600 to-violet-600 text-transparent"
-          >
-            Beyond Boundary
-          </span>
-        </p>
-      </div>
-
-      <div class="mx-auto max-w-7xl md:pt-32 pt-16 px-6 lg:px-8">
-        <div
-          class="mx-auto grid max-w-2xl grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2"
-        >
-          <div class="flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 xl:pr-20">
-            <img
-              class="h-12 self-start"
-              src="https://tailwindui.com/img/logos/tuple-logo-gray-900.svg"
-              alt=""
-            />
-            <figure class="mt-10 flex flex-auto flex-col justify-between">
-              <blockquote class="text-lg leading-8 text-gray-900">
-                <p>
-                  “Amet amet eget scelerisque tellus sit neque faucibus non
-                  eleifend. Integer eu praesent at a. Ornare arcu gravida
-                  natoque erat et cursus tortor consequat at. Vulputate gravida
-                  sociis enim nullam ultricies habitant malesuada lorem ac.
-                  Tincidunt urna dui pellentesque sagittis.”
-                </p>
-              </blockquote>
-              <figcaption class="mt-10 flex items-center gap-x-6">
-                <img
-                  class="h-14 w-14 rounded-full bg-gray-50"
-                  src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-                <div class="text-base">
-                  <div class="font-semibold text-gray-900">Judith Black</div>
-                  <div class="mt-1 text-gray-500">CEO of Tuple</div>
-                </div>
-              </figcaption>
-            </figure>
+          <p>
+            “Qui dolor enim consectetur do et non ex amet culpa sint in ea non
+            dolore. Enim minim magna anim id minim eu cillum sunt dolore
+            aliquip. Amet elit laborum culpa irure incididunt adipisicing culpa
+            amet officia exercitation. Eu non aute velit id velit Lorem elit
+            anim pariatur.”
+          </p>
+        </blockquote>
+        <figcaption class="mt-10 flex items-center gap-x-6">
+          <img
+            class="h-12 w-12 rounded-full bg-gray-50"
+            src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=1024&h=1024&q=80"
+            alt=""
+          />
+          <div class="text-sm leading-6">
+            <div class="font-semibold text-gray-700">Judith Black</div>
+            <div class="mt-0.5 text-gray-600">CEO of Workcation</div>
           </div>
-          <div
-            class="flex flex-col border-t border-gray-900/10 pt-10 sm:pt-16 lg:border-l lg:border-t-0 lg:pl-8 lg:pt-0 xl:pl-20"
-          >
-            <img
-              class="h-12 self-start"
-              src="https://tailwindui.com/img/logos/reform-logo-gray-900.svg"
-              alt=""
-            />
-            <figure class="mt-10 flex flex-auto flex-col justify-between">
-              <blockquote class="text-lg leading-8 text-gray-900">
-                <p>
-                  “Excepteur veniam labore ullamco eiusmod. Pariatur consequat
-                  proident duis dolore nulla veniam reprehenderit nisi officia
-                  voluptate incididunt exercitation exercitation elit. Nostrud
-                  veniam sint dolor nisi ullamco.”
-                </p>
-              </blockquote>
-              <figcaption class="mt-10 flex items-center gap-x-6">
-                <img
-                  class="h-14 w-14 rounded-full bg-gray-50"
-                  src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-                <div class="text-base">
-                  <div class="font-semibold text-gray-900">
-                    Joseph Rodriguez
-                  </div>
-                  <div class="mt-1 text-gray-500">CEO of Reform</div>
-                </div>
-              </figcaption>
-            </figure>
-          </div>
-        </div>
-      </div>
+        </figcaption>
+      </figure>
     </div>
-  </div>
+  </section>
 </template>
