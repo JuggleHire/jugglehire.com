3ffb15c10a8d68a652b2101ef0fdd6283d72f959
shuvo-me <shuvo.js.dev@gmail.com>
2024-01-18 17:16:43 +0600

    updated: authority communication

--- components/AuthorityCommunication.vue
+++ components/AuthorityCommunication.vue
@@ -4,7 +4,7 @@
       <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
         <div class="lg:ml-auto lg:pl-4 lg:pt-4">
           <div class="lg:max-w-lg">
-            <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            <p class="mt-2  text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               Authority Communication.
             </p>
             <p class="mt-6 text-lg leading-8 text-gray-600">
