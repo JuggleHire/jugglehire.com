041bb740627ce67103054923c96dff4fdcfdddf7
Rakib0101 <rakib.01jan@gmail.com>
2024-01-19 12:26:08 +0600

    chore: demo image replace by real image

--- components/RecruitmentPro/FeatureSection6.vue
+++ components/RecruitmentPro/FeatureSection6.vue
@@ -79,7 +79,7 @@
                             </dl>
                         </div>
                     </div>
-                    <img src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                    <img src="/dashboard.png"
                         alt="Product screenshot"
                         class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-white/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
                         width="2432" height="1442">
