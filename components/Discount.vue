70429ab913c5fecf3887da44ae745a037a9173e6
shuvo-me <shuvo.js.dev@gmail.com>
2024-01-19 09:03:29 +0600

    updated: discount section

--- components/Discount.vue
+++ components/Discount.vue
@@ -1,47 +1,55 @@
 <template>
-  <!-- Announcement Banner -->
-  <div class="max-w-[85rem] p-22 mx-auto">
-    <div
-      class="bg-blue-600 bg-[url('https://preline.co/assets/svg/examples/abstract-1.svg')] bg-no-repeat bg-cover bg-center p-4 rounded-lg text-center space-y-8"
-    >
-      <p class="me-2 inline-block text-white font-bold py-4">
-        Secure 24% Discount for New Year Today
-      </p>
-      <div class="flex items-center justify-center gap-3 text-white">
-        <div>
-          <h2 class="text-6xl font-bold">01:</h2>
-          <span>Days</span>
+  <!-- Hero -->
+  <div class="bg-slate-900">
+    <div class="bg-gradient-to-b from-violet-600/[.15] via-transparent">
+      <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-24 space-y-8">
+        <!-- Announcement Banner -->
+        <div class="flex justify-center">
+          <a class="group inline-block bg-white/[.05] hover:bg-white/[.1] border border-white/[.05] p-1 ps-4 rounded-full shadow-md"
+            href="../figma.html">
+            <p class="me-2 inline-block text-white text-sm">
+              Secure 24% Discount for New Year Today
+            </p>
+
+          </a>
         </div>
-        <div>
-          <h2 class="text-6xl font-bold">07:</h2>
-          <span>Minutes</span>
+        <!-- End Announcement Banner -->
+
+        <!-- Title -->
+        <div class="max-w-3xl text-center mx-auto">
+          <div class="flex justify-center gap-3 font-medium text-gray-200 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+            <div>
+              <h2>01:</h2>
+              <span class="text-lg">Days</span>
+            </div>
+            <div>
+              <h2>07:</h2>
+              <span class="text-lg">Minutes</span>
+            </div>
+            <div>
+              <h2>24</h2>
+              <span class="text-lg">Seconds</span>
+            </div>
+          </div>
         </div>
-        <div>
-          <h2 class="text-6xl font-bold">24</h2>
-          <span>Seconds</span>
+        <!-- End Title -->
+
+
+        <!-- Buttons -->
+        <div class="text-center">
+          <a class="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-6 "
+            href="#">
+            Save My Access
+            <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+              viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+              stroke-linejoin="round">
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </a>
         </div>
+        <!-- End Buttons -->
       </div>
-      <a
-        class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-full border-2 border-white text-white hover:border-white/70 hover:text-white/70 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-        href="../figma.html"
-      >
-        Save My Access
-        <svg
-          class="flex-shrink-0 w-4 h-4"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path d="m9 18 6-6-6-6" />
-        </svg>
-      </a>
     </div>
   </div>
-  <!-- End Announcement Banner -->
+  <!-- End Hero -->
 </template>
