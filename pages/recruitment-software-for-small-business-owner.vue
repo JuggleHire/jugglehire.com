4277744c006e64cce71b31f1611bf9e01bfd4c81
Rakib0101 <rakib.01jan@gmail.com>
2024-01-22 16:25:37 +0600

    style: fix faqs questions and answer replace by real qna

--- pages/recruitment-software-for-small-business-owner.vue
+++ pages/recruitment-software-for-small-business-owner.vue
@@ -8,7 +8,7 @@
         <recruitment-pro-feature-section-4 />
         <recruitment-pro-testimonial-section />
         <recruitment-pro-feature-section-5 />
-        <pricing-faq-section title="Got Question?" centerTitle="true" />
+        <pricing-faq-section title="Got Question?" centerTitle="true" :faqs="faqs" />
         <recruitment-pro-feature-section-6 />
     </div>
 </template>
@@ -35,52 +35,36 @@
     })
 
 
-    const posts = [
+    const faqs = [
         {
-            id: 1,
-            title: 'Boost your conversion rate',
-            href: '#',
-            description:
-                'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-            imageUrl:
-                'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-            date: 'Mar 16, 2020',
-            datetime: '2020-03-16',
-            author: {
-                name: 'Michael Foster',
-                imageUrl: 'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-            },
+            question: "What Level of Customer Support Can You Guarantee?",
+            answer:
+                "99.95% of the time, you don't need us. Still, if you feel urgent about anything, just message us, and we'll get back to you within 24 hours or less.",
         },
         {
-            id: 2,
-            title: 'How to use search engine optimization to drive sales',
-            href: '#',
-            description:
-                'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-            imageUrl:
-                'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80',
-            date: 'Mar 16, 2020',
-            datetime: '2020-03-16',
-            author: {
-                name: 'Michael Foster',
-                imageUrl: 'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-            },
+            question: "Can I Pay For The Full Year?",
+            answer:
+                "Yes, you can check the plan and get year-long access. And if you choose the full-year plan, you'll get a 29% discount instant.",
         },
         {
-            id: 2,
-            title: 'Boost your conversion rate',
-            href: '#',
-            description:
-                'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-            imageUrl:
-                'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-            date: 'Mar 16, 2020',
-            datetime: '2020-03-16',
-            author: {
-                name: 'Michael Foster',
-                imageUrl: 'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-            },
+            question: "Can I Use The Software On The Mobile?",
+            answer:
+                "Surely, most of our clients are busy people, and they're unable to sit around in front of a laptop all day long. Instead, our recruitment software makes it all happen in real time over the phone.",
         },
-        // More posts...
+        {
+            question: "What Reporting and Analytics Capabilities Does it Have?",
+            answer:
+                "You'll see exactly how many candidates applied for the specific job post, candidate experience, expertise, location, and everything you need to know to become a master at hiring.",
+        },
+        {
+            question: "How Customized Your Software Is?",
+            answer:
+                "You can juggle through dozens of recruitment software within an hour on the internet. But none of this is exactly designed for small businesses and medium businesses. We bring all the latest technology in one place, so you don't have to wait to become competent at hiring. Install the software, and it takes care of the rest.",
+        },
+        {
+            question: "How useful is it for small and medium business growth?",
+            answer:
+                "Juggle Hire is exactly designed for them. Because many recruiters have zero experience in hiring, they still need to hire the right people. They have to explore multiple tools and boring traditional manual systems. That sometimes cost their fortune. But we bring it all under the same umbrella. So you can hire like a pro even if you are new.",
+        },
     ]
 </script>
