15797f956448344254542a9696ed4f8620940006
shuvo-me <shuvo.js.dev@gmail.com>
2024-01-23 15:55:35 +0600

    updated: easiest-job section

--- components/EasiestJob.vue
+++ components/EasiestJob.vue
@@ -12,7 +12,7 @@
             <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
               <div v-for="feature in featuresChecks" :key="feature.name" class="relative ">
                 <dt class=" font-semibold text-gray-900 flex items-center gap-2">
-                  <span class="text-blue-600">
+                  <span class="text-green-600">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                       <path fill-rule="evenodd"
                         d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
@@ -30,9 +30,9 @@
           class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
           width="2432" height="1442" />
       </div>
-      <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-12 mt-16">
-        <div class="flex group" v-for="(item, index) in cardData" :key="item.title" data-aos="zoom-in"
-          :data-aos-delay="100 * index">
+      <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-12 mt-16 ">
+        <div class="flex group transition duration-200 ease-linear hover:scale-110" v-for="(item, index) in cardData"
+          :key="item.title">
           <div class="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-blue-600 text-white shrink-0">
             <component :is="item.icon" class="h-6 w-6" />
           </div>
