d934ddf97321bc55464ee59f8b89eec6d70f5bec
Rakib0101 <rakib.01jan@gmail.com>
2024-01-23 15:50:06 +0600

    chore: heading section padding top increased

--- components/RecruitmentPro/FeatureSection6.vue
+++ components/RecruitmentPro/FeatureSection6.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="overflow-hidden bg-white py-10 relative">
-
+    <div class="overflow-hidden bg-white pt-10 pb-24 relative">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div
                 class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
