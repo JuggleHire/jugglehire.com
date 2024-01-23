3a0585919cda1c037d9c02b26bf1f199c098f07c
shuvo-me <shuvo.js.dev@gmail.com>
2024-01-23 10:50:53 +0600

    updated: logo cloud

--- components/LogoCloud.vue
+++ components/LogoCloud.vue
@@ -2,10 +2,9 @@
     <div class="bg-white py-24 sm:py-32">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <h2 class="text-center text-lg font-semibold leading-8 text-gray-900" v-text="title" />
-            <div class="mx-auto mt-10 flex flex-wrap justify-center items-center gap-x-4 gap-y-3">
-                <img v-for="client in clients" :key="client.name"
-                    class="h-12 w-auto bg-white px-3 py-2 rounded object-contain" :src="client.logo" :alt="client.name"
-                    width="158" height="48" />
+            <div class="mx-auto mt-10 flex flex-wrap justify-center items-center gap-[5rem]">
+                <img v-for="client in clients" :key="client.name" class="max-h-10 w-auto  object-contain" :src="client.logo"
+                    :alt="client.name" width="158" height="40" />
             </div>
         </div>
     </div>
@@ -37,13 +36,13 @@ const clients = [
         name: 'Pixency - JuggleHire Client',
         logo: '/clients/pixency.png'
     },
-    {
-        name: 'BitByte Technology - JuggleHire Client',
-        logo: '/clients/bitbyte-technology.svg'
-    },
-    {
-        name: 'Microters - JuggleHire Client',
-        logo: '/clients/microters.png'
-    },
+    // {
+    //     name: 'BitByte Technology - JuggleHire Client',
+    //     logo: '/clients/bitbyte-technology.svg'
+    // },
+    // {
+    //     name: 'Microters - JuggleHire Client',
+    //     logo: '/clients/microters.png'
+    // },
 ]
 </script>
